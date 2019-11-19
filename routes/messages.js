85294766270fd3defadf1d48e61db7a0f4bb1e7b
Vitaliy Yemets <vitali8.emez@gmail.com>
2019-11-19 11:49:28 +0200

    deleted logic for example db

--- routes/messages.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const messagesController = require('../controllers/messages');
-
-router.get('/', messagesController.getData);
-router.get('/sequelize', messagesController.getDataS);
-
-module.exports = router;
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const usersController = require('../controllers/users');
-
-router.get('/', usersController.getAll);
-router.get('/:userId', usersController.getUser);
-
-module.exports = router;
